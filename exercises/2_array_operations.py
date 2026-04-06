6e1b2e8db42424f72d38fcd33bf9dc88794bbfff
Ahmed Elsherbiny <ahmedelsherbiny1512@gmail.com>
2026-04-06 15:02:39 +0200

    Add New files for exerciese

---
+++ exercises/2_array_operations.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def main():
+    # Example: create a NumPy array
+    arr = np.array([1, 2, 3])
+    print("NumPy array:", arr)
+
+    print(arr + 1)
+    print(arr * 2)
+    print(arr * 5)
+
+
+if __name__ == "__main__":
+    main()
---
+++ exercises/3_2D_array.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def main():
+    # Example: create a NumPy array
+    matrix = np.array([
+        [1, 2, 3],
+        [4, 6, 8]
+        ])
+    print("NumPy array:", matrix)
+
+    print(matrix[0 , 1])
+
+
+if __name__ == "__main__":
+    main()
